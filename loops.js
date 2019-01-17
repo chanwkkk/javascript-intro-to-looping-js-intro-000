4e070ab172b869f153dfa7ddcc0ef35d33ed3cc5
Yingqi Chen <chanwk18@hotmail.com>
2019-01-17 01:16:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -24,6 +24,6 @@ function doWhileLoop(array){
   do{
     array.shift();
   }
-  while{array.length > 0 && incrementVariable()
-  }return array;
+  while(array.length > 0 && incrementVariable());
+  return array;
 }
