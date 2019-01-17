c6385174b4a881cb12747ec61980451d7158c755
Yingqi Chen <chanwk18@hotmail.com>
2019-01-17 01:08:53 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,10 +15,12 @@ function whileLoop(num){
   }return "done"
 }
 
-function incrementVariable() {
-  i = i + 1;
-}
+
 function doWhileLoop(array){
+  var i=0;
+  function incrementVariable() {
+    i = i + 1;
+  }
   do{
     array.pop();
   }
